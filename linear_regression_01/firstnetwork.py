ce78123d460206a94ed55b1d9292e07b9f5c8550
grahambilley <55400403+grahambilley@users.noreply.github.com>
2024-03-30 08:34:02 -0400

    Update firstnetwork.py

--- linear_regression_01/firstnetwork.py
+++ linear_regression_01/firstnetwork.py
@@ -4,12 +4,38 @@ targets = [2,4,6,8]
 
 # Define model parameters
 w = 0.1     # Weight
+learning_rate = 0.1
+epochs = 25
 
 def predict(i):
     return w*i
 
+# Define cost functions
+# Mean Sum of Errors
+def mse(errors):
+    return sum(errors)/len(errors)
+
 # Train the network
-pred   = [predict(i) for i in inputs]
-errors = [t-p for p, t in zip(pred, targets)]
-cost   = sum(errors)/len(errors)
-print(f'Weight: {w:.2f}, Cost: {cost:.2f}')
+for _ in range(epochs):
+    pred   = [predict(i) for i in inputs]
+    errors = [t-p for p, t in zip(pred, targets)]
+    cost   = mse(errors)
+    print(f'Weight: {w:.2f}, Cost: {cost:.2f}')
+    # Back propagation
+    w += learning_rate*cost
+
+# Test Data
+test_inputs  = [5,6]
+test_targets = [10,12]
+test_preds = [predict(i) for i in test_inputs]
+for i, t, p in zip(test_inputs, test_targets, test_preds):
+    print(f'Inputs: {i}, Targets: {t}, Preds: {p:.4f}')
+
+'''
+Each iteration the predictions are calculated for all training samples.
+The cost is calculated by averaging the errors for all training samples.
+At the end of each iteration, the weight is updated.
+
+i.e. Each iteration, all samples are processed and used to update the weight.
+This is the "batch gradient descent" algorithm.
+'''
