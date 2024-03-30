583104ae931fae52214955e12d636282180f4e21
grahambilley <55400403+grahambilley@users.noreply.github.com>
2024-03-30 08:22:57 -0400

    Update firstnetwork.py

--- linear_regression_01/firstnetwork.py
+++ linear_regression_01/firstnetwork.py
@@ -1,7 +1,15 @@
-# Define model params
-w = 0.1
+# Define training data
+inputs  = [1,2,3,4]
+targets = [2,4,6,8]
+
+# Define model parameters
+w = 0.1     # Weight
 
 def predict(i):
     return w*i
 
-print(predict(2))
+# Train the network
+pred   = [predict(i) for i in inputs]
+errors = [t-p for p, t in zip(pred, targets)]
+cost   = sum(errors)/len(errors)
+print(f'Weight: {w:.2f}, Cost: {cost:.2f}')
